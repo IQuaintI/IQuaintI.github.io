6dfdb40e1e9c9dd05cda4c36a2d62f91f5fee6e3
Quaint <alexgomez2797@gmail.com>
2024-07-05 20:01:54 -0400

    Stuff

--- dist/index.96e2502d.js
+++ dist/index.96e2502d.js
@@ -142,15 +142,16 @@
       this[globalName] = mainExports;
     }
   }
-})({"ielor":[function(require,module,exports) {
+})({"5COXS":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
+var HMR_USE_SSE = false;
 module.bundle.HMR_BUNDLE_ID = "c9b3bd9996e2502d";
 "use strict";
-/* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
+/* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
   HMRAsset,
   HMRMessage,
@@ -189,6 +190,7 @@ declare var HMR_HOST: string;
 declare var HMR_PORT: string;
 declare var HMR_ENV_HASH: string;
 declare var HMR_SECURE: boolean;
+declare var HMR_USE_SSE: boolean;
 declare var chrome: ExtensionContext;
 declare var browser: ExtensionContext;
 declare var __parcel__import__: (string) => Promise<void>;
@@ -232,7 +234,8 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== "undefined") {
         "0.0.0.0"
     ].includes(hostname) ? "wss" : "ws";
     var ws;
-    try {
+    if (HMR_USE_SSE) ws = new EventSource("/__parcel_hmr");
+    else try {
         ws = new WebSocket(protocol + "://" + hostname + (port ? ":" + port : "") + "/");
     } catch (err) {
         if (err.message) console.error(err.message);
@@ -302,12 +305,14 @@ if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== "undefined") {
             }
         }
     };
-    ws.onerror = function(e) {
-        if (e.message) console.error(e.message);
-    };
-    ws.onclose = function() {
-        console.warn("[parcel] \uD83D\uDEA8 Connection to the HMR server was lost");
-    };
+    if (ws instanceof WebSocket) {
+        ws.onerror = function(e) {
+            if (e.message) console.error(e.message);
+        };
+        ws.onclose = function() {
+            console.warn("[parcel] \uD83D\uDEA8 Connection to the HMR server was lost");
+        };
+    }
 }
 function removeErrorOverlay() {
     var overlay = document.getElementById(OVERLAY_ID);
@@ -37502,6 +37507,6 @@ $RefreshReg$(_c, "Card");
 },{"react/jsx-dev-runtime":"iTorj","../css/Card.scss":"h2gb5","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"h2gb5":[function() {},{}],"aKF38":[function(require,module,exports) {
 module.exports = require("96f45e736d121bb7").getBundleURL("hjEE9") + "rxSymbol.f9418452.png" + "?" + Date.now();
 
-},{"96f45e736d121bb7":"lgJ39"}],"irmnC":[function() {},{}]},["ielor","1xC6H","l4cos"], "l4cos", "parcelRequire9205")
+},{"96f45e736d121bb7":"lgJ39"}],"irmnC":[function() {},{}]},["5COXS","1xC6H","l4cos"], "l4cos", "parcelRequire9205")
 
 //# sourceMappingURL=index.96e2502d.js.map
